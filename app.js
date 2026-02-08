e9a7bcd166a312852247b2fa50e56ec2ee16d5e4
root <root@Docker.urj5odcf1hiejfghxdsoos5zng.rx.internal.cloudapp.net>
2026-02-08 05:32:43 +0000

    send all files to git hub

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	  res.send('Hello from Kubernetes DevOps App');
+});
+
+app.get('/health', (req, res) => {
+	  res.status(200).send('OK');
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
+
